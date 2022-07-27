356f5d661c8d02c2673acbdbc71983f17687118b
jgonzcou <jgonzcou@gmail.com>
2022-07-27 15:52:25 +0200

    add license comment info to string files

--- lang/en/block_timestat.php
+++ lang/en/block_timestat.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package    block_timestat
+ * @copyright  2014 Barbara Dębska, Łukasz Sanokowski, Łukasz Musiał
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['blockname'] = 'Timestat';
 $string['pluginname'] = 'Timestat';
 $string['blocktitle'] = 'Timestat';
@@ -12,3 +34,6 @@ $string['hours'] = ' hours ';
 $string['minuts'] = ' minutes ';
 $string['seconds'] = ' seconds ';
 $string['choosetimeperiod'] = 'Choose time period';
+$string['privacy:metadata:block_timestat'] = 'Information about the time spent by the user in a specific log entry.';
+$string['privacy:metadata:block_timestat:log_id'] = 'The ID of the user related to the log entry.';
+$string['privacy:metadata:block_timestat:timespent'] = 'The time spent by the user in the log entry.';
