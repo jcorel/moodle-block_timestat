3ab19e1950e552516fc57915611d4758a2223561
jcorel <86133880+jcorel@users.noreply.github.com>
2024-10-14 19:13:30 +0200

    bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024141000;
+$plugin->version = 2024101400;
 $plugin->component = 'block_timestat';
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->requires = 2021051700;
