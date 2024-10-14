299ba032bedbc3b3cd2ac32626435e07dddbd243
jgonzcou <jgonzcou@gmail.com>
2024-10-14 18:16:58 +0200

    bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024052400;
+$plugin->version = 2024141000;
 $plugin->component = 'block_timestat';
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->requires = 2021051700;
